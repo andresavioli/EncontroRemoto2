975af600108077d94a3b9f8db9b1a7f94f03b64e
André Savioli <aandresavioli@gmail.com>
2022-07-13 16:31:30 -0300

    atividade ER2

---
+++ listaDeAlunos.js
@@ -0,0 +1,64 @@
+let numeroDeAlunos = 10
+
+let contador = 0
+
+
+
+
+
+/*do {if (contador == 0)
+    {
+        console.log ("o número atual é zero.")
+    }
+    else if(contador %2 == 0)
+    {
+        console.log ("o número "+contador+" par")
+    }
+    else
+    {
+        console.log (`O número ${contador} é ímpar`)
+    }
+    contador++
+    
+} while (contador < numeroDeAlunos);*/
+
+
+
+/*while (contador < numeroDeAlunos) 
+{
+    if (contador == 0)
+    {
+        console.log ("o número atual é zero.")
+    }
+    else if(contador %2 == 0)
+    {
+        console.log ("o número "+contador+" par")
+    }
+    else
+    {
+        console.log (`O número ${contador} é ímpar`)
+    }
+    contador++
+}/*
+
+
+
+
+/*
+for (let contador = 0; contador <= numeroDeAlunos; contador++) 
+{   
+    //console.log(contador)
+    if (contador == 0)
+    {
+        console.log ("o número atual é zero.")
+    }
+    else if(contador %2 == 0)
+    {
+        console.log ("o número "+contador+" par")
+    }
+    else
+    {
+        console.log (`O número ${contador} é ímpar`)
+    }
+}   
+   */ 
